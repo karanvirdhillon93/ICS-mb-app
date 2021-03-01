8ed8c4325de1a25becc7addf2ea8172d5edf93d0
karanvirdhillon93 <karanvir.dhillon30@online.camosun.ca>
2021-03-01 21:53:11 +0000

    Json data

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,39 @@ import MsgItem from '../components/MsgItem.js';
 import MsgBoard from '../components/MsgBoard.js';
 import Footer from '../components/Footer.js';
 import { Container, Row, Col } from 'react-bootstrap';
+import ky from 'ky-universal';
 
+//. You now need to pass jsonData into the Home Component as props. Use object destructuring
+export async function getStaticProps() {
+  let jsonData;
+  try {
+  jsonData = await ky('http://10.21.75.56:3004/api/messages').json();
+ 
+  } catch (err) {
+  console.log('API Error: ' + err);
+  }
+  return {
+  props: {
+  jsonData
+  }
+  }
+ }
+ /*
+  In the Home Component's return() statement, pass jsonData to the Component that you're rendering
+that has your State Hook with all the messages. Again, use object destructuring.*/
 
-export default function Home() {
+export default function Home({jsonData}) {
   return (
     <Row>
     <Col>
       <Container>
         <Header />
-        <MsgBoard />
+        <MsgBoard MsgItem={jsonData}/>
         <Footer/>
       </Container>
     </Col>
   </Row>
   )
 }
+
+
