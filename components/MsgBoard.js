ffc883fad3d2bec8dbf08f77cb2cd284f9aac824
karanvirdhillon93 <karanvir.dhillon30@online.camosun.ca>
2021-03-01 21:56:29 +0000

    passing JsonData

--- components/MsgBoard.js
+++ components/MsgBoard.js
@@ -3,17 +3,8 @@ import MsgItem from '../components/MsgItem.js';
 import NewMessageForm from '../components/NewMessageForm.js'
 import { useState } from 'react';
 
-const MsgBoard=()=>{
- /* const messages=[
-    {id:0, Name:"Bill",msgText:"Hi All!"},
-    {id:1, Name:"Ann",msgText:"ICS 211 is fun!"},
-    {id:2, Name:"Johnny",msgText:"I'm stranded !"},
-    {id:3, Name:"Barb",msgText:"Hi"},
-    {id:4, Name:"Frank",msgText:"Who's tired?"},
-    {id:5, Name:"Sarah",msgText:"I heart React"}
-  ]
-*/
-  let [messages,setMessages]=useState([
+const MsgBoard=({jsonData})=>{
+ /*   let [messages,setMessages]=useState([
     {id:0, name:"Bill",msgText:"Hi All!"},
     {id:1, name:"Ann",msgText:"ICS 211 is fun!"},
     {id:2, name:"Johnny",msgText:"I'm stranded !"},
@@ -21,6 +12,8 @@ const MsgBoard=()=>{
     {id:4, name:"Frank",msgText:"Who's tired?"},
     {id:5, name:"Sarah",msgText:"I heart React"}
   ])
+*/
+  let [messages,setMessages]=useState(jsonData)
   // handler for submission of Form in
   // NewMessageForm Component
  
