76ab37bc835dbee15180b8c766cc4e1360821d0d
karanvirdhillon93 <karanvir.dhillon30@online.camosun.ca>
2021-03-02 00:13:37 +0000

    Completionv1.0

--- components/MsgBoard.js
+++ components/MsgBoard.js
@@ -5,41 +5,24 @@ import { useState } from 'react';
 import ky from 'ky-universal';
 
 const MsgBoard=({jsonData})=>{
- /*   let [messages,setMessages]=useState([
-    {id:0, name:"Bill",msgText:"Hi All!"},
-    {id:1, name:"Ann",msgText:"ICS 211 is fun!"},
-    {id:2, name:"Johnny",msgText:"I'm stranded !"},
-    {id:3, name:"Barb",msgText:"Hi"},
-    {id:4, name:"Frank",msgText:"Who's tired?"},
-    {id:5, name:"Sarah",msgText:"I heart React"}
-  ])
-*/
   let [messages,setMessages]=useState(jsonData)
-  // handler for submission of Form in
-  // NewMessageForm Component
- 
-//n AJAX POST Request to your API:
+  //In AJAX POST Request to your API:
   const addNewMessage = async (values) => {
-
     try{
-      const message = await ky.post('http://localhost:3004/api/messages',
+      const message = await ky.post('http://10.21.75.56:3004/api/messages',
       {
        json: values
       }).json();
       console.log(values);
       // values.id = messages.length;
       //messages.unshift(values);
-//Move your existing State Hook update function into the try clause after the POST Request
+      //Move your existing State Hook update function into the try clause after the POST Request
       console.log(messages);
       setMessages( [values, ...messages] );
       console.log(messages)
     }catch(e){
       console.log(e);
-    }
-    /*
-     Comment out the values.id statement in addNewMessage(). Recall that your API will now handle
-    assigning an id to each message*/
-;
+    };
   }
     return(
       <>
@@ -54,7 +37,7 @@ const MsgBoard=({jsonData})=>{
           </thead>
           <tbody>   
           {messages.map( (message, index) =>
- <MsgItem key={ message.id } {...message} msgNum={index+1} />
+ <MsgItem key={ index } {...message} msgNum={index+1} />
 )}
           </tbody>
         </Table>
