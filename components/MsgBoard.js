2711aafd0193923e1c7841b6780b51a05ab99778
Karanvir Dhillon <C0373297@online.camosun.ca>
2021-04-08 19:14:54 +0000

    Updating the New Message Form

--- components/MsgBoard.js
+++ components/MsgBoard.js
@@ -1,14 +1,16 @@
 import Table from 'react-bootstrap/Table';
 import MsgItem from '../components/MsgItem.js';
 import NewMessageForm from '../components/NewMessageForm.js'
-import { useState } from 'react';
+//import { useState } from 'react';
 import ky from 'ky-universal';
 import React from 'react';
+import jwt_decode from 'jwt-decode';
+import { useState, useRef } from 'react';
 //Importing LoginForm
 import LoginForm from '/home/student/ics221-mb-app/components/LoginForm.js'
 
 const MsgBoard=({jsonData})=>{
-
+  const usernameRef = useRef(null);
   //Boolean State Hook to keep track if the User is logged in or not.
   const [isLogged, setIsLogged] = React.useState(false);
   let [messages,setMessages]=useState(jsonData) 
@@ -25,6 +27,8 @@ const MsgBoard=({jsonData})=>{
        }).json();
        console.log(values);
        sessionStorage.setItem('token', isLog.token);
+       const decodedTokenPayload = jwt_decode(isLog.token);
+       usernameRef.current = decodedTokenPayload.username;
     }catch(e){
       console.log(e);
     }
@@ -33,6 +37,7 @@ const MsgBoard=({jsonData})=>{
 
   const addNewMessage = async (values) => {
     try{
+      values.name = usernameRef.current;
       const message = await ky.post(`${process.env.NEXT_PUBLIC_HOST}/api/messages`,
       {
        json: values,
